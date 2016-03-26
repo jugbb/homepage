780d045ce57e76139a38b7ce9d901be2173a45e2
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2016-03-26 13:36:15 +0100

    Skript, um alle Seiten von www.jug-bb.de herunterzuladen wird nicht mehr benoetigt.

--- fetch.bash
+++
@@ -1,5 +0,0 @@
-# http://www.pro-linux.de/artikel/2/624/webseiten-herunterladen-mit-gnu-wget.html
-# http://www.linuxjournal.com/content/downloading-entire-web-site-wget
-
-wget -r -c --domains jug-bb.de --page-requisites --no-clobber \
-    http://www.jug-bb.de
